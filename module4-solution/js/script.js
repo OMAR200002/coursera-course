0ae700e925797db2a1ddc3b61c874cdf198cbf7a
OMAR200002 <omarkhadrouni2017@gmail.com>
2020-07-21 14:57:06 +0200

    module4-solution

---
+++ module4-solution/js/script.js
@@ -0,0 +1,15 @@
+(function() {
+	var names = [ 'Yaakov', 'John', 'Jen', 'Jason', 'Paul', 'Frank', 'Larry', 'Paula', 'Laura', 'Jim' ];
+
+	for (var i = 0; i < names.length; i++) {
+		var str = names[i];
+		var str = str.toLowerCase();
+		var firstLetter = str.charAt(0);
+
+		if (firstLetter == 'j') {
+			byeSpeaker.speak(names[i]);
+		} else {
+			helloSpeaker.speak(names[i]);
+		}
+	}
+})();
